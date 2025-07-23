e7af873e06c2e5f2e0aebce9e29b726537f66f56
fal343 <98967311+fal343@users.noreply.github.com>
2025-07-23 14:49:05 +0100

    fix error

--- src/blocks/PropertyBlock/Component.tsx
+++ src/blocks/PropertyBlock/Component.tsx
@@ -45,11 +45,12 @@ export const PropertyBlock: React.FC<{
               href={`/properties/${property.id}`}
               className="w-full rounded overflow-hidden shadow-lg hover:shadow-xl bg-white transition transform hover:-translate-y-1 flex flex-col"
             >
-              {typeof property.images?.[0]?.image === 'object' &&
+              {property.images?.[0]?.image &&
+                typeof property.images[0].image === 'object' &&
                 'url' in property.images[0].image && (
                   <img
                     className="w-full h-[40vh] object-cover"
-                    src={property.images[0].image.url}
+                    src={(property.images[0].image as { url: string }).url}
                   />
                 )}
               <div className="flex-1 px-6 py-4 flex flex-col justify-between border border-gray-200">
