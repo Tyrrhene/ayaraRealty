d102e4bde290d53bba0ec2336d43209b49867b26
fal343 <98967311+fal343@users.noreply.github.com>
2025-07-23 13:06:51 +0100

    send urls only

--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -3,19 +3,19 @@
 import React, { useState } from 'react'
 
 type ImageCarouselProps = {
-  images: { url: string }[]
+  images: string[]
 }
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
   const [current, setCurrent] = useState(0)
   console.log(images)
-  console.log(current)
+
   return (
     <div className="flex flex-col md:flex-row gap-4">
       {/* Main Image */}
       <div className="flex-1">
         <img
-          src={images[current]?.url || ''}
+          src={images[current]}
           alt={`Image ${current + 1}`}
           className="rounded-lg w-full h-auto object-cover"
         />
@@ -34,11 +34,7 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
             }`}
             onClick={() => setCurrent(index)}
           >
-            <img
-              src={img.url}
-              alt={`Thumbnail ${index + 1}`}
-              className="object-cover w-full h-full"
-            />
+            <img src={img} alt={`Thumbnail ${index + 1}`} className="object-cover w-full h-full" />
           </button>
         ))}
       </div>
