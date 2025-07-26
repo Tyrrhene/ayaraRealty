22c6eb94fb7bcb4363ba39747a3ddbbc8a665077
fal343 <98967311+fal343@users.noreply.github.com>
2025-07-26 20:01:32 +0100

    test deploy

--- src/app/(frontend)/properties/page.tsx
+++ src/app/(frontend)/properties/page.tsx
@@ -1,12 +1,15 @@
-'use client'
+import type { Metadata } from 'next'
+import dynamicImport from 'next/dynamic' // ✅ Rename this
 
-import type { Metadata } from 'next/types'
-import PageClient from './page.client'
+const PageClient = dynamicImport(() => import('./page.client'), { ssr: false })
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+export const metadata: Metadata = {
+  title: 'Payload Website Template Properties',
+}
 
 export default function Page() {
   return <PageClient />
-}
-
-export const metadata: Metadata = {
-  title: `Payload Website Template Properties`,
 }
