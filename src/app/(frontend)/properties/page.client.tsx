0952d2de655a2cce8a5047af06e947e292e2b859
fal343 <98967311+fal343@users.noreply.github.com>
2025-07-26 19:10:15 +0100

    test page

--- src/app/(frontend)/properties/page.client.tsx
+++ src/app/(frontend)/properties/page.client.tsx
@@ -1,47 +1,40 @@
+'use client'
+
 import React, { useEffect, useState } from 'react'
 import { Property } from '@/payload-types'
 import { PropertyBlock } from '@/blocks/PropertyBlock/Component'
+import { PropertiesSearchHeader } from '@/components/PropertiesSearchHeader'
+import { SortDropdown } from '@/components/SortDropdown'
 
 const LIMIT = 24
 
 export default function PageClient() {
   const [properties, setProperties] = useState<Property[]>([])
+  const [filters, setFilters] = useState({
+    bedrooms: 0,
+    development: '',
+    type: '',
+    location: '',
+    minPrice: 0,
+    maxPrice: 0,
+    sortBy: 'default', // Added
+  })
 
-  const fetchProperties = async () => {
-    try {
-      const query = new URLSearchParams({
-        page: '1',
-        limit: String(LIMIT),
-        depth: '3',
-      })
+  console.log('test log')
 
-      const res = await fetch(`/api/properties?${query.toString()}`)
-
-      if (!res.ok) {
-        throw new Error(`Fetch failed with status: ${res.status}`)
-      }
-
-      const data = await res.json()
-      setProperties(data.docs || [])
-    } catch (err) {
-      console.error('Error fetching properties:', err)
-    }
-  }
-
-  useEffect(() => {
-    fetchProperties()
-  }, [])
+  console.log('properties for slug' + properties)
 
   return (
     <div>
+      <PropertiesSearchHeader filters={filters} setFilters={setFilters} />
       <div className="mb-2 mt-6 ml-[40px]">Viewing {properties.length} Homes for Sale</div>
       <div className="mb-6 mt-2 ml-[40px]">
         Showing listings marketed by all brokers in the searched area.
       </div>
-
-      <div>
-        <PropertyBlock properties={properties} />
-      </div>
+      <SortDropdown
+        sortBy={filters.sortBy}
+        setSortBy={(value) => setFilters((prev) => ({ ...prev, sortBy: value }))}
+      />
     </div>
   )
 }
