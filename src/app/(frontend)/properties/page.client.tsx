5c09ec9e6779462ed488f8f67ed9276d133ed81b
fal343 <98967311+fal343@users.noreply.github.com>
2025-07-26 19:05:18 +0100

    test prod

--- src/app/(frontend)/properties/page.client.tsx
+++ src/app/(frontend)/properties/page.client.tsx
@@ -1,24 +1,11 @@
-'use client'
-
 import React, { useEffect, useState } from 'react'
 import { Property } from '@/payload-types'
 import { PropertyBlock } from '@/blocks/PropertyBlock/Component'
-import { PropertiesSearchHeader } from '@/components/PropertiesSearchHeader'
-import { SortDropdown } from '@/components/SortDropdown'
 
 const LIMIT = 24
 
 export default function PageClient() {
   const [properties, setProperties] = useState<Property[]>([])
-  const [filters, setFilters] = useState({
-    bedrooms: 0,
-    development: '',
-    type: '',
-    location: '',
-    minPrice: 0,
-    maxPrice: 0,
-    sortBy: 'default', // Added
-  })
 
   const fetchProperties = async () => {
     try {
@@ -27,15 +14,6 @@ export default function PageClient() {
         limit: String(LIMIT),
         depth: '3',
       })
-
-      if (filters.bedrooms > 0)
-        query.append('where[bedrooms][greater_than_equal]', String(filters.bedrooms))
-
-      if (filters.minPrice > 0)
-        query.append('where[price][greater_than_equal]', String(filters.minPrice))
-
-      if (filters.maxPrice > 0)
-        query.append('where[price][less_than_equal]', String(filters.maxPrice))
 
       const res = await fetch(`/api/properties?${query.toString()}`)
 
@@ -49,25 +27,17 @@ export default function PageClient() {
       console.error('Error fetching properties:', err)
     }
   }
-  console.log('test log')
 
   useEffect(() => {
     fetchProperties()
-  }, [filters])
-
-  console.log('properties for slug' + properties)
+  }, [])
 
   return (
     <div>
-      <PropertiesSearchHeader filters={filters} setFilters={setFilters} />
       <div className="mb-2 mt-6 ml-[40px]">Viewing {properties.length} Homes for Sale</div>
       <div className="mb-6 mt-2 ml-[40px]">
         Showing listings marketed by all brokers in the searched area.
       </div>
-      <SortDropdown
-        sortBy={filters.sortBy}
-        setSortBy={(value) => setFilters((prev) => ({ ...prev, sortBy: value }))}
-      />
 
       <div>
         <PropertyBlock properties={properties} />
