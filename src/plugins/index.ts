877fe6670e841c8111ed6851885cb306063ef30e
fal343 <98967311+fal343@users.noreply.github.com>
2025-08-11 18:03:02 +0100

    more changes

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -15,7 +15,7 @@ import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title ? `${doc.title} | Ayara Realty` : 'Ayara Realty'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
