dc17ae9b0eab45ed880c65537de6fdef59a55359
fal343 <98967311+fal343@users.noreply.github.com>
2025-08-11 15:52:35 +0100

    update meta

--- src/endpoints/seed/home-static.ts
+++ src/endpoints/seed/home-static.ts
@@ -19,7 +19,7 @@ export const homeStatic: RequiredDataFromCollectionSlug<'pages'> = {
                 format: 0,
                 mode: 'normal',
                 style: '',
-                text: 'Payload Website Template',
+                text: 'Ayara Realty',
                 version: 1,
               },
             ],
@@ -80,7 +80,7 @@ export const homeStatic: RequiredDataFromCollectionSlug<'pages'> = {
     },
   },
   meta: {
-    description: 'An open-source website built with Payload and Next.js.',
+    description: 'Selling real estate in Pattaya, Thailand',
     title: 'Ayara Realty',
   },
   title: 'Home',
