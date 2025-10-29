b7f1b475c7f58d00a849cb48a4abd187fa71995d
fal343 <98967311+fal343@users.noreply.github.com>
2025-10-29 11:09:11 +0000

    convert prices to baht

--- src/blocks/PropertyBlock/Component.tsx
+++ src/blocks/PropertyBlock/Component.tsx
@@ -98,7 +98,7 @@ export const PropertyBlock: React.FC<{
                         </p>
                       </div>
                     </div>
-                    <p className="text-2xl font-extrabold">${property.price.toLocaleString()}</p>
+                    <p className="text-2xl font-extrabold">฿{property.price.toLocaleString()}</p>
                   </div>
                 </div>
               </Link>
